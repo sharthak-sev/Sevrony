f4c246c2407f540463e02ca3c353d7ee894460d4
sharthak-sev <sharthakjaiswal50@gmail.com>
2026-06-24 06:19:53 +0530

    feat: add scroll-to-top button to Mistakes Log

--- app.js
+++ app.js
@@ -2773,6 +2773,9 @@ function renderTestReview() {
           </div>
         </div>
       </div>
+      <button class="primary-btn scroll-top-btn" type="button" data-action="scroll-top" aria-label="Scroll to top">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
+      </button>
       </div>
     `;
   }
